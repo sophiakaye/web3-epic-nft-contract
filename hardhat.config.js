c95f628e63645367c40fa641361fbaf319eefa59
Sophia KAYA <sophiakaya@Sophias-MacBook-Pro.local>
2022-08-16 22:10:48 +0300

    init

---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+require('@nomiclabs/hardhat-waffle');
+require("dotenv").config({ path: ".env" });
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    rinkeby: {
+      url: process.env.QUICKNODE_API_KEY_URL,
+      accounts: [process.env.RINKEBY_PRIVATE_KEY],
+    },
+  }
+};
